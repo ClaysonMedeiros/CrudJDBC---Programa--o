f4d3c22798c6ff29019185e455c6c647a5f424d4
clayson_medeiros <clayson_medeiros@10.1.48.186>
2013-08-01 17:27:01 -0300

    Inicializando Projeto

---
+++ src/br/com/senai/view/MarcaTableModel.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senai.view;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author clayson_medeiros
+ */
+public class MarcaTableModel extends AbstractTableModel {
+
+    private static final int COL_ID = 0;
+    private static final int COL_NOME = 1;
+    private List<Marca> linhas;
+    private String[] colunas = {"Id", "Nome"};
+
+    @Override
+    public int getRowCount() {
+        return linhas.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Marca m = linhas.getClass(row);
+
+        if (columnIndex == COL_ID) {
+            return m.getId();
+        } else (COL_NOME == COL_NOME)
+        
+        
+        {
+            return m.getNome();
+        }
+    }
+}
+
